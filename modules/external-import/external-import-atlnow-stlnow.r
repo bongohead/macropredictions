b78762d1f46abc24aba305c3a05a6ca690ddfc1d
Charles Ye <charles@cmefi.com>
2023-11-14 21:09:55 -0500

    Added external import modules (v3.0.0)

---
+++ modules/external-import/external-import-atlnow-stlnow.r
@@ -0,0 +1,125 @@
+# Initialize ----------------------------------------------------------
+# If T, adds data to the database that belongs to data vintages already existing in the database.
+# Set to F only when there are model updates, new variable pulls, or old vintages are unreliable.
+STORE_NEW_ONLY = T
+validation_log <<- list()
+
+## Load Libs ----------------------------------------------------------
+library(macropredictions)
+library(tidyverse)
+
+## Load Env & Logging ----------------------------------------------------------
+load_env()
+pg = connect_pg()
+
+# Import ------------------------------------------------------------------
+
+## Get GDP Release Dates ---------------------------------------------------
+hist_data =
+	get_query(pg, sql(
+		"SELECT val.varname, val.vdate, val.date, val.value
+		FROM forecast_hist_values_v2 val
+		INNER JOIN forecast_variables v ON v.varname = val.varname
+		WHERE form = 'd1' AND  v.varname = 'gdp'"
+		)) %>%
+	group_by(., date) %>%
+	{inner_join(
+		filter(., vdate == min(vdate)) %>% transmute(., date, first_vdate = vdate, first_value = value),
+		filter(., vdate == max(vdate)) %>% transmute(., date, final_vdate = vdate, final_value = value),
+		by = 'date'
+		)} %>%
+	ungroup(.) %>%
+	arrange(., date)
+
+## Get ATL + STL Forecasts ---------------------------------------------------
+local({
+
+	api_key = Sys.getenv('FRED_API_KEY')
+
+	fred_sources = tribble(
+		~ forecast, ~ varname, ~ fred_key,
+		'atlnow', 'gdp','GDPNOW',
+		'atlnow', 'pce', 'PCENOW',
+		'stlnow', 'gdp', 'STLENI'
+	)
+
+	fred_data = lapply(df_to_list(fred_sources), function(x) {
+		get_fred_obs_with_vintage(list(c(x$fred_key, 'q')), api_key, .obs_start = '2020-01-01') %>%
+			filter(., date >= as_date('2020-01-01')) %>%
+			transmute(
+				.,
+				forecast = x$forecast,
+				form = 'd1',
+				freq = 'q',
+				varname = x$varname,
+				vdate = vintage_date,
+				date,
+				value
+				)
+		}) %>%
+		list_rbind(.)
+
+	fred_data <<- fred_data
+})
+
+# ## Get NYF Forecasts ---------------------------------------------------
+# local({
+#
+# 	nyf_url = 'https://www.newyorkfed.org/medialibrary/media/research/policy/nowcast/new-york-fed-staff-nowcast_data_2002-present.xlsx?la=en'
+# 	download.file(nyf_url, file.path(tempdir(), 'nyfnow.xlsx'), mode = 'wb', quiet = TRUE)
+#
+# 	nyf_data =
+# 		readxl::read_excel(file.path(tempdir(), 'nyfnow.xlsx'), sheet = 2, skip = 13) %>%
+# 		rename(., vdate = 1) %>%
+# 		mutate(., vdate = as_date(vdate)) %>%
+# 		pivot_longer(., -vdate, names_to = 'date', values_to = 'value', values_drop_na = T) %>%
+# 		mutate(., date = from_pretty_date(date, 'q')) %>%
+# 		filter(., date >= as_date('2020-01-01')) %>%
+# 		transmute(
+# 			.,
+# 			forecast = 'nyfnow',
+# 			form = 'd1',
+# 			freq = 'q',
+# 			varname = 'gdp',
+# 			vdate,
+# 			date,
+# 			value
+# 			)
+#
+# 	nyf_data <<- nyf_data
+# })
+
+
+## Join and Validate ---------------------------------------------------
+local({
+
+	raw_data = bind_rows(
+		fred_data
+		# nyf_data
+	)
+
+	raw_data %>%
+		group_by(., forecast) %>%
+		filter(., vdate == max(vdate)) %>%
+		ungroup(.) %>%
+		print(.)
+
+	raw_data <<- raw_data
+})
+
+## Export Forecasts ------------------------------------------------------------------
+local({
+
+	# Store in SQL
+	model_values = transmute(raw_data, forecast, form, vdate, freq, varname, date, value)
+
+	rows_added = store_forecast_values_v2(pg, model_values, .store_new_only = STORE_NEW_ONLY, .verbose = T)
+
+	# Log
+	validation_log$store_new_only <<- STORE_NEW_ONLY
+	validation_log$rows_added <<- rows_added
+	validation_log$last_vdate <<- max(raw_data$vdate)
+
+	disconnect_db(pg)
+})
+
---
+++ modules/external-import/external-import-fnma.r
@@ -0,0 +1,222 @@
+# Initialize ----------------------------------------------------------
+# If T, adds data to the database that belongs to data vintages already existing in the database.
+# Set to F only when there are model updates, new variable pulls, or old vintages are unreliable.
+STORE_NEW_ONLY = T
+validation_log <<- list()
+
+## Load Libs ----------------------------------------------------------
+library(macropredictions)
+library(tidyverse)
+library(httr2)
+library(rvest)
+library(reticulate)
+
+use_virtualenv(file.path(Sys.getenv('MP_DIR'), '.venv'))
+
+## Load Connection Info ----------------------------------------------------------
+load_env()
+pg = connect_pg()
+
+# Import ------------------------------------------------------------------
+
+## Get Data ------------------------------------------------------------------
+local({
+
+	message('***** Downloading Fannie Mae Data')
+
+	fnma_dir = file.path(tempdir(), 'fnma')
+	fs::dir_create(fnma_dir)
+
+	message('***** FNMA dir: ', fnma_dir)
+
+	fnma_links =
+		request('https://www.fanniemae.com/research-and-insights/forecast/forecast-monthly-archive') %>%
+		req_perform %>%
+		resp_body_html %>%
+		html_nodes('div.fm-accordion ul li a') %>%
+		keep(., \(x) str_detect(html_text(x), 'News Release| Forecast')) %>%
+		map_dfr(., \(x) tibble(
+			url = str_trim(html_attr(x, 'href')),
+			type = case_when(
+				str_detect(html_text(x), 'News Release') ~ 'article',
+				str_detect(html_text(x), 'Economic Forecast') ~ 'econ_forecast',
+				str_detect(html_text(x), 'Housing Forecast') ~ 'housing_forecast',
+				TRUE ~ NA_character_
+			)
+		)) %>%
+		na.omit
+
+	fnma_details =
+		fnma_links %>%
+		mutate(., group = (1:nrow(.) - 1) %/% 3) %>%
+		pivot_wider(., id_cols = group, names_from = type, values_from = url) %>%
+		na.omit %>%
+		df_to_list %>%
+		.[1:12] %>%
+		imap(., function(x, i) {
+
+			# if (i %% 10 == 0) message('Downloading ', i)
+
+			vdate =
+				request(paste0('https://www.fanniemae.com', x$article)) %>%
+				req_perform %>%
+				resp_body_html %>%
+				html_node(., "div[data-block-plugin-id='field_block:node:news:field_date'] > div") %>%
+				html_text %>%
+				mdy
+
+			request(paste0('https://www.fanniemae.com', x$econ_forecast)) %>%
+				req_perform(., path = file.path(fnma_dir, paste0('econ_', vdate, '.pdf')))
+
+			request(paste0('https://www.fanniemae.com', x$housing_forecast)) %>%
+				req_perform(., path = file.path(fnma_dir, paste0('housing_', vdate, '.pdf')))
+
+			tibble(
+				vdate = vdate,
+				econ_forecast_path = normalizePath(file.path(fnma_dir, paste0('econ_', vdate, '.pdf'))),
+				housing_forecast_path = normalizePath(file.path(fnma_dir, paste0('housing_', vdate, '.pdf')))
+			)
+		}) %>%
+		list_rbind(.)
+
+	camelot = import('camelot')
+
+	fnma_clean_macro = imap(df_to_list(fnma_details), function(x, i) {
+
+		message(str_glue('Importing macro {i}'))
+
+		raw_import = camelot$read_pdf(
+			x$econ_forecast_path,
+			pages = '1',
+			flavor = 'stream',
+			# Below needed to prevent split wrapping columns correctly https://www.fanniemae.com/media/42376/display
+			column_tol = -2,
+			edge_tol = 60
+			#table_areas = list('100, 490, 700, 250')
+			)[0]$df %>%
+			as_tibble(.)
+
+		col_names =
+			df_to_list(raw_import[3, ])[[1]] %>%
+			str_replace_all(., coll(c('.' = 'Q'))) %>%
+			paste0('20', .) %>%
+			{ifelse(. == '20', 'varname', .)}
+
+		clean_import =
+			raw_import[4:nrow(raw_import), ] %>%
+			set_names(., col_names) %>%
+			pivot_longer(., -varname, names_to = 'date', values_to = 'value') %>%
+			filter(., str_length(date) == 6 & str_detect(date, 'Q') & value != '') %>%
+			mutate(
+				.,
+				varname = case_when(
+					str_detect(varname, 'Gross Domestic Product') ~ 'gdp',
+					str_detect(varname, 'Personal Consumption') ~ 'pce',
+					str_detect(varname, 'Residential Fixed Investment') ~ 'pdir',
+					str_detect(varname, 'Business Fixed Investment') ~ 'pdin',
+					str_detect(varname, 'Government Consumption') ~ 'govt',
+					str_detect(varname, 'Net Exports') ~ 'nx',
+					str_detect(varname, 'Change in Business Inventories') ~ 'cbi',
+					str_detect(varname, 'Consumer Price Index') & !str_detect(varname, 'Core') ~ 'cpi',
+					str_detect(varname, 'PCE Chain Price Index') & !str_detect(varname, 'Core') ~ 'pcepi',
+					str_detect(varname, 'Unemployment Rate') ~ 'unemp',
+					str_detect(varname, 'Federal Funds Rate') ~ 'ffr',
+					str_detect(varname, '1-Year Treasury') ~ 't01y',
+					str_detect(varname, '10-Year Treasury') ~ 't10y'
+				),
+				date = from_pretty_date(date, 'q'),
+				value = as.numeric(str_replace_all(value, c(',' = '')))
+			) %>%
+			na.omit(.) %>%
+			transmute(., vdate = as_date(x$vdate), varname, date, value)
+
+		if (length(unique(clean_import$varname)) < 12) stop('Missing variable')
+
+		return(clean_import)
+		}) %>%
+		list_rbind
+
+	fnma_clean_housing = imap(df_to_list(fnma_details), function(x, i) {
+
+		message(str_glue('Importing housing {i}'))
+
+		raw_import = camelot$read_pdf(
+			x$housing_forecast_path,
+			pages = '1',
+			flavor = 'stream',
+			column_tol = -1,
+			edge_tol = 60
+			)[0]$df %>%
+			as_tibble(.)
+
+		col_names =
+			df_to_list(raw_import[3, ])[[1]] %>%
+			str_replace_all(., coll(c('.' = 'Q'))) %>%
+			paste0('20', .) %>%
+			{ifelse(. == '20', 'varname', .)}
+
+		clean_import =
+			raw_import[4:nrow(raw_import), ] %>%
+			set_names(., col_names) %>%
+			pivot_longer(., -varname, names_to = 'date', values_to = 'value') %>%
+			filter(., str_length(date) == 6 & str_detect(date, 'Q') & value != '') %>%
+			mutate(
+				.,
+				varname = case_when(
+					str_detect(varname, 'Total Housing Starts') ~ 'houst',
+					# str_detect(varname, 'Total Home Sales') ~ 'hsold',
+					str_detect(varname, '30-Year') ~ 'mort30y',
+					str_detect(varname, '5-Year') ~ 'mort05y' # Dropped 5 year forecast
+				),
+				date = from_pretty_date(date, 'q'),
+				value = as.numeric(str_replace_all(value, c(',' = '')))
+			) %>%
+			na.omit(.) %>%
+			transmute(., vdate = as_date(x$vdate), varname, date, value)
+		# As of Oct. 2022, 5 year mortgage forecast is dropped so only requires 2
+		if (length(unique(clean_import$varname)) < 2) stop('Missing variable')
+
+		return(clean_import)
+		}) %>%
+		list_rbind
+
+	fnma_data =
+		bind_rows(fnma_clean_macro, fnma_clean_housing) %>%
+		transmute(
+			.,
+			forecast = 'fnma',
+			form = 'd1',
+			freq = 'q',
+			varname,
+			vdate,
+			date,
+			value
+		)
+
+	message('***** Missing Variables:')
+	message(
+		c('gdp', 'pce', 'pdir', 'pdin', 'govt', 'nx', 'cbi', 'cpi', 'pcepi', 'unemp', 'ffr', 't01y', 't10y',
+			'houst', 'mort30y', 'mort05y') %>%
+			keep(., ~ !. %in% unique(fnma_data$varname)) %>%
+			paste0(., collapse = ' | ')
+		)
+
+	raw_data <<- fnma_data
+})
+
+
+## Export Forecast ------------------------------------------------------------------
+local({
+
+	# Store in SQL
+	model_values = transmute(raw_data, forecast, form, vdate, freq, varname, date, value)
+
+	rows_added = store_forecast_values_v2(pg, model_values, .store_new_only = STORE_NEW_ONLY, .verbose = T)
+
+	# Log
+	validation_log$store_new_only <<- STORE_NEW_ONLY
+	validation_log$rows_added <<- rows_added
+	validation_log$last_vdate <<- max(raw_data$vdate)
+
+	disconnect_db(pg)
+})
