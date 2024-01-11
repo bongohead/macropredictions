69602b485648cc9d35cc7e103e6e16c7750de290
Charles Ye <charles@cmefi.com>
2024-01-10 19:59:13 -0500

    Fixed media scrape (v3.0.1)

--- r_helpers/R/scrape-helpers-media.r
+++ r_helpers/R/scrape-helpers-media.r
@@ -17,8 +17,17 @@
 #'  (e.g., the top posts by week for the "dogs" subreddit).
 #'
 #' @examples
-#' get_reddit_data_by_board('StockMarket', freq = 'day', ua = reddit_ua, token = reddit_token, max_pages = 1)
-#'
+#' reddit_token = get_reddit_token(
+#'  ua = Sys.getenv('REDDIT_UA'), id = Sys.getenv('REDDIT_ID'), secret = Sys.getenv('REDDIT_SECRET'),
+#'  username = Sys.getenv('REDDIT_USERNAME'), password = Sys.getenv('REDDIT_PASSWORD')
+#' )
+#' get_reddit_data_by_board(
+#'  'StockMarket',
+#'  freq = 'day',
+#'  ua = Sys.getenv('REDDIT_UA'),
+#'  token = reddit_token,
+#'  max_pages = 1
+#' )
 #'
 #' @import dplyr purrr
 #' @importFrom httr2 request req_headers req_perform resp_headers resp_body_json
@@ -33,7 +42,8 @@ get_reddit_data_by_board = function(subreddit, freq, ua, token, max_pages = 10, 
 		is_scalar_character(token),
 		is_scalar_double(min_upvotes) | is_scalar_integer(min_upvotes),
 		is_scalar_double(min_upvotes) | is_scalar_integer(min_upvotes),
-		is_scalar_logical(.verbose)
+		is_scalar_logical(.verbose),
+		all(map_lgl(c(subreddit, freq, ua, token), \(x) nchar(x) > 0))
 	)
 
 	# Only top possible for top
@@ -80,3 +90,69 @@ get_reddit_data_by_board = function(subreddit, freq, ua, token, max_pages = 10, 
 
 }
 
+#' Returns the auth token for Reddit
+#'
+#' @param ua The `User-Agent` to pass as a header, should be of the form `<platform>:<app ID>:<version string>
+#'  (by u/<Reddit username>`. See [https://praw.readthedocs.io/en/stable/getting_started/quick_start.html].
+#' @param id The Reddit secret ID (received when creating a new API token).
+#' @param secret The Reddit secret key (received when creating a new API token).
+#' @param username The Reddit username used to create the API token.
+#' @param password The Reddit password used to create the API token.
+#' @param .verbose Logging verbosity; defaults to FALSE.
+#'
+#' @return An auth token
+#'
+#' @description
+#' Get a session auth token used for running Reddit API calls
+#'
+#' @examples
+#' reddit_token = get_reddit_token(
+#'  ua = Sys.getenv('REDDIT_UA'), id = Sys.getenv('REDDIT_ID'), secret = Sys.getenv('REDDIT_SECRET'),
+#'  username = Sys.getenv('REDDIT_USERNAME'), password = Sys.getenv('REDDIT_PASSWORD')
+#' )
+#' get_reddit_data_by_board(
+#'  'StockMarket',
+#'  freq = 'day',
+#'  ua = Sys.getenv('REDDIT_UA'),
+#'  token = reddit_token,
+#'  max_pages = 1
+#' )
+#'
+#' @import dplyr purrr
+#' @importFrom httr2 request req_headers req_perform resp_headers resp_body_json
+#' @importFrom RCurl base64
+#'
+#' @export
+get_reddit_token = function(ua, id, secret, username, password) {
+	stopifnot(
+		is_scalar_character(ua),
+		is_scalar_character(id),
+		is_scalar_character(secret),
+		is_scalar_character(username),
+		is_scalar_character(password),
+		all(map_lgl(c(ua, id, secret, username, password), \(x) nchar(x) > 0))
+	)
+	reddit_token =
+		request('https://www.reddit.com/api/v1/access_token') %>%
+		req_headers(
+			'User-Agent' = ua,
+			'Authorization' = paste0(
+				'Basic ',
+				base64(
+					txt = paste0(id, ':', secret),
+					mode = 'character'
+				)
+			)
+		) %>%
+		req_body_form(
+			grant_type = 'client_credentials',
+			username = username,
+			password = password
+			) %>%
+		req_perform() %>%
+		resp_body_json() %>%
+		.$access_token
+
+	return(reddit_token)
+
+}
---
+++ r_helpers/tests/testthat/test-scrape-helpers-media.r
@@ -0,0 +1,11 @@
+test_that('Async function', {
+	
+	skip_if_offline('catfact.ninja')
+	
+	req_list = map(1:5, function(x) request('https://catfact.ninja/fact'))
+	
+	resp_list = send_async_requests(req_list)
+	
+	expect_length(resp_list, 5)
+	expect_length(map_chr(resp_list, function(x) resp_body_json(x)$fact), 5)
+})
