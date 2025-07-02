6555f98f7ae8d3a62b7395701b1d50fa60d6c569
Charles Ye <charles@cmefi.com>
2025-07-02 09:13:54 -0400

    Bugfix

---
+++ install_packages.sh
@@ -0,0 +1,13 @@
+# Incomplete as of 6/30/25
+pip install pandas
+pip install numpy
+pip install asyncio
+pip install aiohttp
+
+# Scraping
+pip install beautifulsoup4
+pip install pillow
+
+# Playwright for browser automation
+pip install playwright
+playwright install --with-deps
